77dc6a745c8934c0c240454704a7649dbf8dba7e
Caique <caiquemorales20@gmail.com>
2023-07-07 03:46:56 -0300

    :heavy_plus_sign: build: firebase

---
+++ src/global/GlobalStyle.ts
@@ -0,0 +1,26 @@
+// Imports
+import { createGlobalStyle } from "styled-components";
+
+// Styled Components
+export const GlobalStyle = createGlobalStyle`
+  /* ==================== HTML Reseting ==================== */
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body{
+    line-height: 1.6;
+  }
+  h1,h2,h3,h4,h5,h6{
+    line-height: 1;
+  }
+  img{
+    width: 100%;
+    height: auto;
+  }
+  a{
+    display: inline-block;
+    text-decoration: none;
+  }
+`;
--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,13 @@ import ReactDOM from "react-dom/client";
 // App
 import App from "./App.tsx";
 
+// Styles
+import { GlobalStyle } from "./global/GlobalStyle.ts";
+
 // Rendering
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
+		<GlobalStyle />
 		<App />
 	</React.StrictMode>
 );
